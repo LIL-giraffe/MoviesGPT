434274f262fe9cc5056ce6047224b09dc09666c2
LIL-giraffe <hdbest33@gmail.com>
2023-10-17 21:24:47 +0530

    Update

--- moviegpt/src/App.js
+++ moviegpt/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="text-3xl font-bold underline text-green-700 ">
+      Hello there
     </div>
   );
 }
